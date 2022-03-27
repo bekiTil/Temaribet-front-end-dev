509bffdec7c4856639c9b1f5e4224128b4d0f5e8
Bereket_Tilahun <btilahun287@gmail.com>
2022-03-27 20:26:47 +0300

    update feedback

--- TemariBet/nextjs-blog/components/Feedback.js
+++ TemariBet/nextjs-blog/components/Feedback.js
@@ -1,26 +1,77 @@
 import { Col, Row } from 'antd';
 const Feedback = () => {
     return (
-        <div className="py-10 ">
+        // <div className="py-10 ">
 
-            <Row gutter={[16, 16]} style={{ padding:"5%",background: "#F0F8FF" }} >
-                <Col xl={24} xs={24} >
-                    <h1 className="text-4xl text-bold text-black pb-2 ">Why my parents love temaribet</h1>
-                </Col>
-                <Col xl={12} xs={24}>
-                    <h1 className="text-2xl text-thin text-blue pb-2 ">Abinet Legesse</h1>
-                    <h1 className="text-xl text-thin text-gray pb-2 ">Mother</h1>
-                    <p>loremNon magna nulla et aute sunt non commodo et. Minim nostrud adipisicing sint occaecat pariatur Lorem do cupidatat. Cillum pariatur et irure sit enim dolor incididunt do exercitation qui ea aute irure. Dolor aliqua velit reprehenderit labore.</p>
-                </Col>
-                <Col xl={12} xs={24}>
-                    <h1 className="text-2xl text-thin text-blue pb-2 ">Abinet Legesse</h1>
-                    <h1 className="text-xl text-thin text-gray pb-2 ">Mother</h1>
-                    <p>loremNon magna nulla et aute sunt non commodo et. Minim nostrud adipisicing sint occaecat pariatur Lorem do cupidatat. Cillum pariatur et irure sit enim dolor incididunt do exercitation qui ea aute irure. Dolor aliqua velit reprehenderit labore.</p>
-                </Col>
-            </Row>
-        </div>
+        //     <Row gutter={[16, 16]} style={{ padding:"5%",background: "#F0F8FF" }} >
+        //         <Col xl={24} xs={24} >
+        //             <h1 className="text-4xl text-bold text-black pb-2 ">Why my parents love temaribet</h1>
+        //         </Col>
+        //         <Col xl={12} xs={24}>
+        //             <h1 className="text-2xl text-thin text-blue pb-2 ">Abinet Legesse</h1>
+        //             <h1 className="text-xl text-thin text-gray pb-2 ">Mother</h1>
+        //             <p>loremNon magna nulla et aute sunt non commodo et. Minim nostrud adipisicing sint occaecat pariatur Lorem do cupidatat. Cillum pariatur et irure sit enim dolor incididunt do exercitation qui ea aute irure. Dolor aliqua velit reprehenderit labore.</p>
+        //         </Col>
+        //         <Col xl={12} xs={24}>
+        //             <h1 className="text-2xl text-thin text-blue pb-2 ">Abinet Legesse</h1>
+        //             <h1 className="text-xl text-thin text-gray pb-2 ">Mother</h1>
+        //             <p>loremNon magna nulla et aute sunt non commodo et. Minim nostrud adipisicing sint occaecat pariatur Lorem do cupidatat. Cillum pariatur et irure sit enim dolor incididunt do exercitation qui ea aute irure. Dolor aliqua velit reprehenderit labore.</p>
+        //         </Col>
+        //     </Row>
+        // </div>
+        <section className="bg-yellow-50 md:bg-gray-100">
+            <div className="container  py-12 mx-auto h-full">
+                <div className="mt-7 mx-auto">
+                    <h2 className="font-bold text-4xl  md:text-5xl lg:text-6px ">Why parents love <span className="text-yellow-300">temaribet</span></h2>
+                </div>
 
-        
+                <div className="px-12 mt-20 mb-36">
+                    <div className="lg:flex mt-7">
+                        <div className="w-full  lg:w-1/2">
+                            <div className="p-8 lg:p-12">
+                                <div>
+                                    <p className="font-bold text-2xl">Abinet Legesse</p>
+                                    <p className="-mt-6 lg:-mt-4 text-xl">Father</p>
+                                </div>
+
+                                <p className="mt-10 text-xl">
+                                    It was difficult to explain to them how the diagnosis of
+                                    certain death had actually given him life. While everyone
+                                    around him was in tears and upset, he actually felt more at
+                                    ease. The doctor said it would be less than a year. That gave
+                                    him a year to live, something he'd failed to do with his daily
+                                    drudgery of a routine that had passed as life until then. It
+                                    was difficult to explain to them how the diagnosis of certain
+                                    death had actually given him life. While everyone
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="mt-1 lg:mt-0 lg:flex-1 lg:border-l border-gray-500">
+                            <div className="p-8 lg:p-12 lg:ml-5">
+                                <div>
+                                    <p className="font-bold text-2xl">Hananel Girma</p>
+                                    <p className="-mt-6 lg:-mt-4 text-xl">Mother</p>
+                                </div>
+                                <p className="mt-10 text-xl">
+                                    It was difficult to explain to them how the diagnosis of
+                                    certain death had actually given him life. While everyone
+                                    around him was in tears and upset, he actually felt more at
+                                    ease. The doctor said it would be less than a year. That gave
+                                    him a year to live, something he'd failed to do with his daily
+                                    drudgery of a routine that had passed as life until then. It
+                                    was difficult to explain to them how the diagnosis of certain
+                                    death had actually given him life. While everyone
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div></div>
+            </div>
+        </section>
+
     )
 }
 export default Feedback;
