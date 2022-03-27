42479b48f5dddd22be4b69d63d40aff8dbb21d87
Duressa Jemal <duressajemal3@gmail.com>
2022-03-28 00:31:50 +0300

    Updated getTutor page

--- TemariBet/nextjs-blog/components/Another.js
+++ TemariBet/nextjs-blog/components/Another.js
@@ -1,31 +1,65 @@
-import { Button,Col,Row } from 'antd';
-import 'antd/dist/antd.css';
+import { Button, Col, Row } from "antd";
+import "antd/dist/antd.css";
 const Another = () => {
-    return (
-        <Row gutter={[8, 8]}>
-            <Col xs={24} xl={12}>
-                <div class=" px-32  justify-center">
-                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-3-800x800.jpg" alt="..." class="rounded-full shadow-lg  max-w-full h-auto align-middle border-none" />
-                </div>
+  return (
+    // <Row gutter={[8, 8]}>
+    //     <Col xs={24} xl={12}>
+    //         <div class=" px-32  justify-center">
+    //             <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-3-800x800.jpg" alt="..." class="rounded-full shadow-lg  max-w-full h-auto align-middle border-none" />
+    //         </div>
 
-            </Col>
-            <Col xs={24} xl={12}>
-                <div className="pt-6 pb-3">
-                    <h1 className="text-7xl text-blue-900">We Prepare For Life.</h1>
-                    <h1 className=" text-7xl text-white">Not just exam</h1>
-                </div>
-                <h3 className="text-2xl text-blue-900">
-                    Top tutors from all over Addis Abeba
-                    <br />
-                    managed by Temaribet
-                </h3>
-                <br />
-                <div className="p-3 items-center"><Button style={{ background: "#1E2F97", borderColor: "#1E2F97" }} type="primary" size="large" shape="round">Get tutor</Button></div>
+    //     </Col>
+    //     <Col xs={24} xl={12}>
+    //         <div className="pt-6 pb-3">
+    //             <h1 className="text-7xl text-blue-900">We Prepare For Life.</h1>
+    //             <h1 className=" text-7xl text-white">Not just exam</h1>
+    //         </div>
+    //         <h3 className="text-2xl text-blue-900">
+    //             Top tutors from all over Addis Abeba
+    //             <br />
+    //             managed by Temaribet
+    //         </h3>
+    //         <br />
+    //         <div className="p-3 items-center"><Button style={{ background: "#1E2F97", borderColor: "#1E2F97" }} type="primary" size="large" shape="round">Get tutor</Button></div>
 
-            </Col>
+    //     </Col>
 
-        </Row>
+    // </Row>
 
-    )
-}
+    <section className="bg-white dark:bg-gray-800 font-serif">
+      <div className="container px-12 py-16 mx-auto">
+        <div className="items-center lg:flex">
+          <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+            <img
+              className="w-full h-full max-w-md rounded-full"
+              src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-3-800x800.jpg"
+              alt="#"
+            />
+          </div>
+
+          <div className="w-full lg:w-1/2 ">
+            <div className="lg:max-w-lg mt-20 lg:mt-0">
+              <h3 className="text-5xl text-center lg:text-left font-bold text-blue-900">
+                Get the perfect tutor
+              </h3>
+
+              <h3 className="text-yellow-400 text-center lg:text-left text-5xl font-bold -mt-3">
+                for your child
+              </h3>
+
+              <p className="text-2xl text-center lg:text-left text-blue-900">
+                Top tutors from all over Addis Ababa managed by Temaribet.
+              </p>
+              <div className = "text-center lg:text-left">
+                <button class=" px-12 py-3 rounded-full mt-20 text-4xl font-bold text-white  transition-colors duration-200 transform bg-blue-900 lg:w-auto hover:bg-blue-700  focus:outline-none focus:bg-blue-600">
+                  Get tutor
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Another;
--- TemariBet/nextjs-blog/components/Header.js
+++ TemariBet/nextjs-blog/components/Header.js
@@ -1,81 +1,81 @@
-
-import {useState} from "react";
+import { useState } from "react";
 import React from "react";
-const Header = () => {
-    const [navbarOpen, setNavbarOpen] = React.useState(false);
-    return (
-        <nav className="overflow-x-hidden fixed top-0 left-0 right-0 z-50 flex flex-wrap items-center justify-between px-2 py-0 bg-yellow-300 ">
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                    <a
-                        className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-blue-900"
-                        href="#"
-                    >
-                        temaribet
-                    </a>
-                    <button
-                        className=" text-gray-500 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                        type="button"
-                        onClick={() => setNavbarOpen(!navbarOpen)}
-                    >
-                        <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                            <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                        </svg>
-                    </button>
-                </div>
-                <div
-                    className={
-                        "lg:flex flex-grow items-center" +
-                        (navbarOpen ? " flex" : " hidden")
-                    }
-                    
-                >
-                    <ul className="pt-4 flex flex-col lg:flex-row list-none lg:ml-auto">
-                        <li className="nav-item h-full">
-                            <a
-                                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
-                                href="#"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item h-full">
-                        <a
-                                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
-                                href="#"
-                            >
-                                About Us
-                            </a>
-                        </li>
-                        <li className="nav-item h-full">
-                        <a
-                                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
-                                href="#"
-                            >
-                                Pricing
-                            </a>
-                        </li>
-                        <li className="nav-item h-full">
-                        <a
-                                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75"
-                                href="#"
-                            >
-                                Login
-                            </a>
-                        </li>
-                        <li className="pl-2  nav-item h-full">
-                        <button className="rounded-full px-10 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-900  hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-        Register
-    </button>
-                        </li>
-                    </ul>
-                    
-    
-                    
-                </div>
-            </div>
-        </nav>
 
-    )
-}
+const Header = () => {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <nav className="overflow-x-hidden fixed top-0 left-0 right-0 z-50 flex flex-wrap items-center justify-between px-2 py-0 bg-yellow-400/75">
+      <div className="container px-0 flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <a
+            className="text-5xl font-black font-fugasOne leading-relaxed inline-block ml-12 mr-4 py-2 whitespace-nowrap text-white"
+            href="#"
+          >
+            temaribet
+          </a>
+          <button
+            className=" text-gray-500 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className={
+            "lg:flex flex-grow items-center" +
+            (navbarOpen ? " flex" : " hidden")
+          }
+        >
+          <ul className="pt-4 font-serif font-bold  flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="nav-item h-full">
+              <a
+                className="px-3 py-2 flex items-center  text-2xl text-blue-900 opacity-40 font-bold leading-snug  hover:opacity-75"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item h-full">
+              <a
+                className="px-3 py-2 ml-5 flex items-center  text-2xl text-blue-900 opacity-40 font-bold leading-snug  hover:opacity-75"
+                href="#"
+              >
+                About Us
+              </a>
+            </li>
+            <li className="nav-item h-full">
+              <a
+                className="px-3 py-2 ml-5 flex items-center text-2xl text-blue-900 opacity-40  font-bold leading-snug  hover:opacity-75"
+                href="#"
+              >
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item h-full">
+              <a
+                className="px-3 py-2 ml-5  flex items-center  text-2xl text-blue-900 opacity-40 font-bold leading-snug  hover:opacity-75"
+                href="#"
+              >
+                Login
+              </a>
+            </li>
+
+            <li className="pl-5 nav-item h-full">
+              <button className="rounded-full px-10 py-2  tracking-wide text-2xl text-white capitalize transition-colors duration-200 transform bg-blue-900  hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                Register
+              </button>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Header;
