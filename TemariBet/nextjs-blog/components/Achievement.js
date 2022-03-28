28f2f40b7b66aa470a3a77bf03e61117e5ee2439
Elsai Deribu <elsaideribu7@gmail.com>
2022-03-28 11:15:18 +0300

    statistics and achievement component edited

--- TemariBet/nextjs-blog/components/Achievement.js
+++ TemariBet/nextjs-blog/components/Achievement.js
@@ -2,27 +2,24 @@ import { Button,List, Col, Row } from 'antd';
 import Stats from "./Statistics.js";
 const Achievement=()=>{
     return (
-        <div className="p-6 bg-gradient-to-r from-blue-900 via-blue-900 to-black ">
+        <div className="p-10 pb-20 bg-gradient-to-r font-serif from-indigo-900 via-black-900 to-black ">
+            <Col   xs={24}  sm={20} md={10}  xl={6} style={{justifyContent:"center"}}>
             <div className="p-7">
-            <h1 className="text-5xl text-bold text-white pb-2 "> We are the little angel of your kids but we can <br/>
-            only help you if you let us </h1>
-             <p className="text-xl text-white">loremAdipisicing incididunt labore cillum sit sunt ullamco anim ea co
-             nsequat laboris proident consectetur. Id eiusmod sint ut dolor deserunt. Ullam
-             co anim laborum pariatur sunt pariatur culpa ad dolore irure occaecat consequat.
-              Exercitation aute officia occaecat proident sunt tempor exercitation. Consectetur
-               velit reprehenderit ipsum in aliquip non reprehenderit dolor in. Veniam qui tempor ullam
-               co sint veniam sit magna sunt labore sunt laborum. Do reprehenderit eiusmod aliqua officia
-                magna anim pariatur duis incididunt deserunt pariatur nostrud cupidatat veniamDolor ipsum mollit labore non exercitation. Mollit in esse aute qui est. Ea occaecat mollit officia incididunt commodo ex nulla aute mollit consectetur ad cupidatat non in. Sit laborum anim proident non nisi ut duis minim est duis reprehenderit cupidatat. Ullamco nulla qui culpa eu quis non ut ad
-                ipisicing eiusmod deserunt elit Lorem excepteur.a.</p>
-             </div>
-             <Row gutter={[16,16]}>
-                 <Stats text = "FAMILIES"  numbers = "350+"></Stats>
-                 <Stats text = "SCHOOLS"  numbers = "50+"></Stats>
-                 <Stats text = "TUTORS"  numbers = "10,000+"></Stats>
-                 <Stats text = "VISION"  numbers = "1"></Stats>
-             </Row>
-             
-
+            <h1 className="text-7xl text-bold text-white p-8 pt-4 pb-1 pr-44 "> We are the little angel of your kids but we can
+            only help if you let us </h1>
+            <p className="text-4xl text-gray-400 p-8 pt-0 pr-96">loremAdipisicing incididunt labore cillum sit sunt ullamco anim ea co
+            nsequat laboris proident consectetur. Id eiusmod sint ut dolor deserunt. Ullam
+            magna anim pariatur duis incididunt  elit Lorem excepteur.a.</p>
+            </div>
+            </Col>
+        
+        
+            <Row gutter={[16,16]}>
+                <Stats text = "FAMILIES"  numbers = "350+"></Stats>
+                <Stats text = "SCHOOLS"  numbers = "50+"></Stats>
+                <Stats text = "TUTORS"  numbers = "10,000+"></Stats>
+                <Stats text = "VISION"  numbers = "1"></Stats>
+            </Row>
         </div>
     )
 }
--- TemariBet/nextjs-blog/components/Statistics.js
+++ TemariBet/nextjs-blog/components/Statistics.js
@@ -3,9 +3,12 @@ import { Button,List, Col, Row } from 'antd';
 
 function Stats({text, numbers}) {
   return (
-<Col xs={24}  sm={20} md={10}  xl={6} style={{justifyContent:"center"}}>
-    <div className="text-6xl text-center text-bold text-white"> {numbers}</div>
-    <div className="text-4xl text-center  text-white"> {text}</div>
+<Col   xs={24}  sm={20} md={10}  xl={6} style={{justifyContent:"center"}}>
+  <div className='mb-40'>
+  <div className="text-8xl text-center text-bold text-white "> {numbers}</div>
+    <div className="text-4xl text-center pt-10 text-left  text-gray-400  text-white"> <span>{text}</span> </div>
+  </div>
+
 </Col>
   )
 }
