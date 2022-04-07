00f20f6101e5ef617a019d04038098ee9d3bf9b4
Bereket_Tilahun <btilahun287@gmail.com>
2022-04-08 02:52:37 +0300

    new

--- TemariBet/nextjs-blog/components/Parent/child_components/page9.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page9.js
@@ -1,70 +1,70 @@
 const Page9 = () => {
   return (
-    <div className = "bg-[#FED607]">
+    <div className = "bg-[#FED607] font-minionPro">
       <div>
-        <h1>In what area does your child</h1>
-        <p>need tutoring</p>
+        <h1 className="text-3xl text-[#1A3765] font-bold " >In what area does your child</h1>
+        <p className="text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">need tutoring</p>
       </div>
 
-      <div>
+      <div className="text-center md:text-left" >
         <input type="radio" value="" name="" id="all" />
 
-        <label for="all">All subjectsin grade 6 </label>
+        <label className="text-xl text-[#5A5A5A]  font-semibold  pl-0 p-2" for="all">All subjects 2 in grade 6 </label>
         <br></br>
       </div>
 
       <div>
-        <div className = "mt-4">
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+        <div className = "flex flex-row mt-4">
+          <button className=" flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Math
           </button>
 
-          <button className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className="flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Phyiscs
           </button>
 
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className="flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Chemistry
           </button>
         </div>
 
-        <div className="my-4">
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+        <div className="flex flex-row my-4">
+          <button className=" flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Biology
           </button>
 
-          <button className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className="flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             History
           </button>
 
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className="flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Geography
           </button>
         </div>
 
-        <div>
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+        <div className="flex flex-row my-4">
+          <button className=" flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Economics
           </button>
 
-          <button className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className="flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             Civics
           </button>
 
-          <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+          <button className=" flex-1 mr-2 bg-white hover:bg-blue-700 hover:text-white text-blue-900 font-bold py-3 px-6 ">
             SAT
           </button>
         </div>
       </div>
 
-      <div>
-        <button className=" my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 ">
+      <div className = "mt-8 text-center md:text-left">
+        <button className=" bg-[#1A3765] hover:bg-blue-700 text-white text-2xl font-semibold py-2 px-6 ">
           + Add subject
         </button>
       </div>
 
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+      <div className = "mt-8 text-center md:text-left">
+        <button className=" bg-[#1A3765] hover:bg-blue-700 text-white text-3xl font-semibold py-3 px-6 ">
           CONTINUE
         </button>
       </div>
