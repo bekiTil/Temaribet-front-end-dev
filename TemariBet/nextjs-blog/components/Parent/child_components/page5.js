2cdcd2c5037fa43207b3420d179111db181e9892
Duressa Jemal <duressajemal3@gmail.com>
2022-04-08 02:53:55 +0300

    updated page1

--- TemariBet/nextjs-blog/components/Parent/child_components/page5.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page5.js
@@ -1,7 +1,7 @@
 const Page5 = () => {
   return (<div class="flex flex-col justify-between ">
     <div className = "bg-[#FED607]">
-      <div>
+      <div className = "text-center lg:text-left">
         <h1 className="text-2xl  md:text-3xl text-[#1A3765] font-semibold 2xl:pr-30">Now! Tell us about your child's need</h1>
         <p className=" text-lg md:text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">So that we provide a tailor made solution</p>
       </div>
@@ -18,19 +18,4 @@ const Page5 = () => {
 
 export default Page5;
 
-      <form class="form-horizontal w-3/4 mx-auto" method="POST" action="#">
-                              <div class="flex flex-col mt-4">
-                                  <input id="email" type="text" class="flex-grow h-8 px-2 border rounded border-grey-400" name="email" value="" placeholder="Email" />
-                              </div>
-                              <div class="flex flex-col mt-4">
-                                  <input id="password" type="password" class="flex-grow h-8 px-2 rounded border border-grey-400" name="password" required placeholder="Password" />
-                              </div>
-                              <div class="flex items-center mt-4">
-                                  <input type="checkbox" name="remember" id="remember" class="mr-2" /> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
-                              </div>
-                              <div class="flex flex-col mt-8">
-                                  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
-                                      Login
-                                  </button>
-                              </div>
-                          </form> 
+      
