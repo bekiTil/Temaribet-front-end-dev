d79760a4fb97a9c4bf138b66a7e8abb78d5151fb
Duressa Jemal <duressajemal3@gmail.com>
2022-04-08 02:38:48 +0300

    updated page3

--- TemariBet/nextjs-blog/components/Parent/child_components/page2.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page2.js
@@ -1,25 +1,34 @@
 const Page2 = () => {
-    return (
-      <div className = "bg-[#FED607]">
-        <div>
-          <h1 className = "text-4xl font-semibold">I AM YONATAN</h1>
-          <p className = "text-2xl">I am coordinator at Temaribet</p>
-        </div>
-  
-        <div className = "mt-36">
-          <label className = "tracking-wide font-semibold text-2xl" for="phoneN">What is your name?</label>
-          <br></br>
-          <input className = "mt-4 py-5 px-24" type="text" id="phoneN" placeHolder="First name &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &#124; &nbsp; &nbsp; &nbsp;   Last name" width = "50"/>
-        </div>
-  
-        <div className = "mt-8">
+  return (
+    <div className="bg-[#FED607]">
+      <div>
+        <h1 className="text-3xl text-[#1A3765] font-bold ">I AM YONATAN.</h1>
+        <p className="text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">
+          I am coordinator at Temaribet
+        </p>
+      </div>
+      
+      <div className="mt-36">
+        <label className="tracking-wide font-semibold text-2xl" for="phoneN">
+          What is your name?
+        </label>
+        <br></br>
+        <input
+          className="mt-4 py-5 px-24"
+          type="text"
+          id="phoneN"
+          placeHolder="First name &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &#124; &nbsp; &nbsp; &nbsp;   Last name"
+          width="50"
+        />
+      </div>
+
+      <div className = "mt-8 text-center md:text-left">
           <button className=" bg-[#1A3765] hover:bg-blue-700 text-white text-3xl font-semibold py-3 px-6 ">
             CONTINUE
           </button>
         </div>
-      </div>
-    );
-  };
-  
-  export default Page2;
-  
+    </div>
+  );
+};
+
+export default Page2;
--- TemariBet/nextjs-blog/components/Parent/child_components/page3.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page3.js
@@ -2,18 +2,18 @@ const Page3 = () => {
   return (
     <div className = "bg-[#FED607]">
       <div>
-        <h1>Let us Know you a little bit.</h1>
-        <p>So that we give you the service you deserve.</p>
+        <h1 className="text-center lg:text-left text-3xl text-[#1A3765] font-bold ">This is our phone number.</h1>
+        <p className="text-center lg:text-left text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">+251940120591</p>
       </div>
 
-      <div>
-        <label for="phoneN">Your phone number?</label>
+      <div className="mt-28 text-center lg:text-left" >
+        <label className="tracking-wide font-semibold text-2xl " for="phoneN">Your phone number?</label>
         <br></br>
-        <input type="text" id="phoneN" placeHolder="Phone no"></input>
+        <input className="mt-4 py-5 px-20 lg:px-24" type="text" id="phoneN" placeHolder="+251&nbsp;  &nbsp; &nbsp; &nbsp; &#124; &nbsp; &nbsp;Phone number "></input>
       </div>
 
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+      <div className = "mt-8 text-center lg:text-left">
+        <button className="bg-[#1A3765] hover:bg-blue-700 text-white text-3xl font-semibold py-3 px-6 ">
           CONTINUE
         </button>
       </div>
