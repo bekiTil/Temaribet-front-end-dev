4843c2d20ceb494ec92db266227d1a05b737b73e
Elsai Deribu <elsaideribu7@gmail.com>
2022-04-08 01:52:19 +0300

    edited the parents page 5 and 6

--- TemariBet/nextjs-blog/components/Parent/Main.js
+++ TemariBet/nextjs-blog/components/Parent/Main.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Page1 from "./child_components/page1";
 function Main(props) {
-  return <div class="flex rounded-lg shadow-lg w-full  bg-white sm:mx-0" style={{height: "680px"}}>
+  return <div class="font-minionPro flex rounded-lg shadow-lg w-full  bg-white sm:mx-0" style={{height: "680px"}}>
               <div class="flex flex-col w-full  lg:w-1/2 xl:w-3/5  py-16 px-14 bg-[#FED607]">
                   <div class="flex flex-col flex-1 mb-8">
                       
                       <div class="w-full ">
-        
+
                           {props.children}
-                          
+
                       </div>
                   </div>
               </div>
--- TemariBet/nextjs-blog/components/Parent/child_components/page6.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page6.js
@@ -2,26 +2,26 @@ const Page6 = () => {
   return (
     <div className = "bg-[#FED607]">
       <div>
-        <h1>I AM YONATAN</h1>
-        <p>I am coordinator at Temaribet</p>
+        <h1 className="text-3xl text-[#1A3765] font-semibold ">I AM YONATAN.</h1>
+        <p className="text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">I am coordinator at Temaribet</p>
       </div>
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ">
+      <div className=" flex justify-start">
+        <button className=" text-5xl bg-white text-white text-[#1A3765] font-bold mr-4 my-6 px-10 py-6">
           Girl
         </button>
 
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-5">
+        <button className="text-5xl bg-white text-white text-[#707070] font-bold ml-4 my-6  px-10 py-6">
           Boy
         </button>
       </div>
-      <div>
-        <label for="name">What is child's name?</label>
+      <div className="text-[#1A3765] text-2xl  font-bold">
+        <label  for="name">What is child's name?</label>
         <br></br>
         <input type="text" id="name" placeHolder="name"></input>
       </div>
 
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+        <button className=" text-3xl bg-[#1A3765] text-white font-bold my-6 py-4 px-6 ">
           CONTINUE
         </button>
       </div>
