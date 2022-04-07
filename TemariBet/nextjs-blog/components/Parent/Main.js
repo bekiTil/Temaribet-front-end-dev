7d441d468490f84b6ce2a5aa471b48455fa74dad
Elsai Deribu <elsaideribu7@gmail.com>
2022-04-08 00:38:44 +0300

    deleted justify from parents main component

--- TemariBet/nextjs-blog/components/Parent/Main.js
+++ TemariBet/nextjs-blog/components/Parent/Main.js
@@ -3,25 +3,10 @@ import Page1 from "./child_components/page1";
 function Main(props) {
   return <div class="flex rounded-lg shadow-lg w-full  bg-white sm:mx-0" style={{height: "680px"}}>
               <div class="flex flex-col w-full  lg:w-1/2 xl:w-3/5  p-4 bg-[#FED607]">
-                  <div class="flex flex-col flex-1 justify-center mb-8">
+                  <div class="flex flex-col flex-1 mb-8">
                       
                       <div class="w-full mt-4">
-                          {/* <form class="form-horizontal w-3/4 mx-auto" method="POST" action="#">
-                              <div class="flex flex-col mt-4">
-                                  <input id="email" type="text" class="flex-grow h-8 px-2 border rounded border-grey-400" name="email" value="" placeholder="Email" />
-                              </div>
-                              <div class="flex flex-col mt-4">
-                                  <input id="password" type="password" class="flex-grow h-8 px-2 rounded border border-grey-400" name="password" required placeholder="Password" />
-                              </div>
-                              <div class="flex items-center mt-4">
-                                  <input type="checkbox" name="remember" id="remember" class="mr-2" /> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
-                              </div>
-                              <div class="flex flex-col mt-8">
-                                  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
-                                      Login
-                                  </button>
-                              </div>
-                          </form> */}
+                   
                           {props.children}
 
                           
--- TemariBet/nextjs-blog/components/Parent/child_components/page5.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page5.js
@@ -1,18 +1,36 @@
 const Page5 = () => {
-  return (
+  return (<div class="flex flex-col justify-between mt-4">
     <div className = "bg-[#FED607]">
       <div>
-        <h1>Now! Tell us about your child's need</h1>
+        <h1 className="font-bold text-lg">Now! Tell us about your child's need</h1>
         <p>So that we provide a tailor made solution</p>
       </div>
 
       <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-10 px-10 ">
-          Ready
+        <button className="bg-[#1A3765] hover:bg-blue-700 text-white text-4xl font-bold px-7 py-16  rounded-full ">
+          Ready?
         </button>
       </div>
+    </div>
     </div>
   );
 };
 
 export default Page5;
+
+      <form class="form-horizontal w-3/4 mx-auto" method="POST" action="#">
+                              <div class="flex flex-col mt-4">
+                                  <input id="email" type="text" class="flex-grow h-8 px-2 border rounded border-grey-400" name="email" value="" placeholder="Email" />
+                              </div>
+                              <div class="flex flex-col mt-4">
+                                  <input id="password" type="password" class="flex-grow h-8 px-2 rounded border border-grey-400" name="password" required placeholder="Password" />
+                              </div>
+                              <div class="flex items-center mt-4">
+                                  <input type="checkbox" name="remember" id="remember" class="mr-2" /> <label for="remember" class="text-sm text-grey-dark">Remember Me</label>
+                              </div>
+                              <div class="flex flex-col mt-8">
+                                  <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
+                                      Login
+                                  </button>
+                              </div>
+                          </form> 
