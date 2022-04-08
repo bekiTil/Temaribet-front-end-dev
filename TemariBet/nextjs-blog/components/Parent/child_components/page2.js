210b1cdb223b494f7f274b17ddab062a5d1c4f98
Elsai Deribu <elsaideribu7@gmail.com>
2022-04-08 12:11:04 +0300

    edited page 4 of parents registration page

--- TemariBet/nextjs-blog/components/Parent/child_components/page2.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page2.js
@@ -1,20 +1,20 @@
 const Page2 = () => {
   return (
     <div className="bg-[#FED607]">
-      <div>
+      <div className="text-center lg:text-left">
         <h1 className="text-3xl text-[#1A3765] font-bold ">I AM YONATAN.</h1>
         <p className="text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">
           I am coordinator at Temaribet
         </p>
       </div>
       
-      <div className="mt-36">
+      <div className="mt-36 text-center lg:text-left">
         <label className="tracking-wide font-semibold text-2xl" for="phoneN">
           What is your name?
         </label>
         <br></br>
         <input
-          className="mt-4 py-5 px-24"
+          className="mt-4 py-5 px-20 lg:px-24"
           type="text"
           id="phoneN"
           placeHolder="First name &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &#124; &nbsp; &nbsp; &nbsp;   Last name"
@@ -22,7 +22,7 @@ const Page2 = () => {
         />
       </div>
 
-      <div className = "mt-8 text-center md:text-left">
+      <div className = "mt-8 text-center lg:text-left">
           <button className=" bg-[#1A3765] hover:bg-blue-700 text-white text-3xl font-semibold py-3 px-6 ">
             CONTINUE
           </button>
--- TemariBet/nextjs-blog/components/Parent/child_components/page4.js
+++ TemariBet/nextjs-blog/components/Parent/child_components/page4.js
@@ -1,19 +1,19 @@
 const Page4 = () => {
   return (
-    <div className = "bg-[#FED607]">
+<div className = "bg-[#FED607]">
       <div>
-        <h1>Our office is located at</h1>
-        <p>Office number, 206 Dream bldg.CMC</p>
+        <h1 className="text-center lg:text-left text-3xl text-[#1A3765] font-bold ">Our office is located at</h1>
+        <p className="text-center lg:text-left text-2xl text-[#5A5A5A] font-semibold  pl-0 p-2">Office number, 206 Dream bldg, CMC</p>
       </div>
 
-      <div>
-        <label for="address">Your location</label>
+      <div className="mt-28 text-center lg:text-left" >
+        <label className="tracking-wide  text-[#1A3765] font-semibold text-2xl " for="phoneN">Your location</label>
         <br></br>
-        <input type="text" id="address" placeHolder="Adress"></input>
+        <input className="mt-4 py-5 px-20 lg:px-24" type="text" id="phoneN" placeHolder="Addis Ababa&nbsp;  &nbsp; &nbsp; &nbsp; &#124; &nbsp; &nbsp;Adress "></input>
       </div>
 
-      <div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 ">
+      <div className = "mt-8 text-center lg:text-left">
+        <button className="bg-[#1A3765] hover:bg-blue-700 text-white text-3xl font-semibold py-3 px-6 ">
           CONTINUE
         </button>
       </div>
